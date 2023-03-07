01726eb3522a0536bee75a6f451a2ed723dfae5e
AmelieLT <amelie_letron@yahoo.fr>
2023-03-07 15:50:01 +0100

    AuthController

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+    }
+}
