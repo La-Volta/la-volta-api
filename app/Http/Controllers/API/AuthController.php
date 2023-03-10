82aec357c07c1e761d81d381f749574581307a33
bgiudicid <b.giudicid@gmail.com>
2023-03-10 11:10:04 +0100

    login almost there

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -44,4 +44,53 @@ class AuthController extends Controller
         }
     }
 
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'=>'required|max:191',
+            'password'=>'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'validation_errors'=>$validator->messages(),
+            ]);
+        }
+        else
+        {
+            $user = User::where('email', $request->email)->first();
+
+            if(! $user || ! Hash::check($request->password, $user->password))
+            {
+                return response()->json([
+                    'status'=>401,
+                    'message'=>'Invalid Credentials',
+                ]);
+            }
+            else
+            {
+                if($user->role_as == 1) //1= Admin
+                {
+                    $role = 'admin';
+                    $token = $user->createToken($user->email.'_AdminToken', ['server:admin'])->plainTextToken;
+                }
+                else
+                {
+                    $role = '';
+                    $token = $user->createToken($user->email.'_Token', [''])->plainTextToken;
+                }
+
+                return response()->json([
+                    'status'=>200,
+                    'username'=>$user->name,
+                    'token'=>$token,
+                    'message'=>'Logged In Successfully',
+                    'role'=>$role,
+                ]);
+            }
+        }
+    }
+
+
 }
