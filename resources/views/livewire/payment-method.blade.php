675e3da548117caee48c169ee53b08845031133c
maricarmen <mchuecooviedo@gmail.com>
2023-03-20 11:13:33 +0100

    stripepay update2

--- resources/views/livewire/payment-method.blade.php
+++ resources/views/livewire/payment-method.blade.php
@@ -1,22 +1,29 @@
 <div>
-    <input id="card-holder-name" type="text">
- 
 
-    <div id="card-element"></div>
- 
-<button id="card-button" data-secret="{{ $intent->client_secret }}">
-    Update Payment Method
-</button>
+    <div class="bg-red rounded shadow-lg">
+        <div class="px-8 py-6">
+                <input id="card-holder-name" class="form-pay-stripe">
+            
+                <div id="card-element" class="form-pay-stripe"></div>
+                <label className="">Nombre del titular de la tarjeta</label>
+            <button id="card-button" data-secret="{{ $intent->client_secret }}">
+                Update Payment Method
+            </button>
+        </div>
+    </div>
 
-    <script src="https://js.stripe.com/v3/"></script>
- 
-    <script>
-    const stripe = Stripe('stripe-public-key');
- 
-    const elements = stripe.elements();
-    const cardElement = elements.create('card');
- 
-    cardElement.mount('#card-element');
-    </script>
+        @push('js')
 
+            <script src="https://js.stripe.com/v3/"></script>
+        
+            <script>
+            const stripe = Stripe('stripe-public-key');
+        
+            const elements = stripe.elements();
+            const cardElement = elements.create('card');
+        
+            cardElement.mount('#card-element');
+            </script>
+
+        @endpush
 </div>
